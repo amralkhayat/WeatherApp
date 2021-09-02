b96b25b673bdd87e028b7c654d39517b48e35118
amr <amralkhayat0@gmail.com>
2021-09-02 10:13:06 +0200

    add CacheWeatherGetway class

--- WeatherApp/Gateway/Cashe/CacheWeatherGateway.swift
+++ WeatherApp/Gateway/Cashe/CacheWeatherGateway.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+
+
+//MARK:- CacheWeatherGetway
+//this class responsible to compose api and local persistence to be able to make api request and cash it to the realm database.
+class CacheWeatherGetway: WeatherGateway  {
+    
+    
+    let localPersistenceWeatherGateway: LocalPersistenceMethods
+    
+    let apiWeatherGateway:  APIWeatherGateway
+    
+    init(apiWeatherGateway:  APIWeatherGateway, localPersistenceWeathersGateway: LocalPersistenceMethods) {
+        
+        self.apiWeatherGateway = apiWeatherGateway
+        
+        self.localPersistenceWeatherGateway =  localPersistenceWeathersGateway
+    }
+    
+    
+    func fetchWeather(completionHandler: @escaping WeatherGatewayCompletionHandler<WeatherListModel>) {
+        apiWeatherGateway.fetchWeather { (result) in
+            
+            self.handleFetchWeatherApiResult(result, completionHandler: completionHandler)
+        
+        }
+    }
+    
+    
+    func handleFetchWeatherApiResult(_ result: Result<WeatherListModel, Error>, completionHandler: @escaping (Result<WeatherListModel,Error>) -> Void) {
+        
+        switch result {
+        
+        case let .success(Weather):
+       
+            handlingSuccessSavetoRelam(Weather: Weather, completionHandler: completionHandler)
+            
+        case .failure(_):
+            
+            localPersistenceWeatherGateway.fetchWeather(completionHandler: completionHandler)
+        }
+        }
+    
+
+    private func handlingSuccessSavetoRelam(Weather: WeatherListModel,completionHandler: @escaping (Result<WeatherListModel,Error>) -> Void){
+        localPersistenceWeatherGateway.addWeather(object: Weather) { result in
+        
+            switch result {
+
+            case .success(_):
+                
+                self.localPersistenceWeatherGateway.fetchWeather(completionHandler: completionHandler)
+                
+            case .failure(_):
+                self.localPersistenceWeatherGateway.fetchWeather(completionHandler: completionHandler)
+            }
+            
+          }
+        
+       }
+    
+      }
+    
